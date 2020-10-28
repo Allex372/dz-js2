fc357b00d1b19060f30f49f2c9ca9bad382a9707
Allex372 <62441924+Allex372@users.noreply.github.com>
2020-10-29 01:23:12 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,157 @@
+// const mass1=[1,2,3,4,5];
+// const mass2=['Allex', 'Maxim', 'Sofia', 'Julia', 'Nazar'];
+// const mass3=['allex', 4,5,true,false];
+// console.log(mass1);
+// console.log(mass2);
+// console.log(mass3);
+
+// const mass = [1,2,3,4,5];
+// console.log(mass[4]);
+
+///////////////
+// НЕЗНАЮ///////////////
+
+// const massDiv = ['<div>text</div>','<div>2</div>','<div>3</div>','<div>4</div>','<div>5</div>','<div>6</div>','<div>7</div>','<div>8</div>','<div>9</div>','<div>10</div>']
+//
+// for (let i=0; i<massDiv.length; i++){
+//     document.write(massDiv[i])
+// }
+
+// const massDiv = ['<div>text</div>','<div>2</div>','<div>3</div>','<div>4</div>','<div>5</div>','<div>6</div>','<div>7</div>','<div>8</div>','<div>9</div>','<div>10</div>']
+//
+// for (let i=0; i<massDiv.length; i++){
+//     document.write(massDiv[i])
+//     document.write(i+'<br>')
+//     document.write('************')
+// }
+
+// const massH = ['<h1>1</h1>','<h1>2</h1>','<h1>3</h1>','<h1>4</h1>','<h1>5</h1>','<h1>6</h1>','<h1>7</h1>','<h1>8</h1>','<h1>9</h1>','<h1>10</h1>','<h1>11</h1>','<h1>12</h1>','<h1>13</h1>','<h1>14</h1>','<h1>15</h1>','<h1>16</h1>','<h1>17</h1>','<h1>18</h1>','<h1>19</h1>','<h1>20</h1>']
+//
+// let i = 0;
+//
+// while (i<massH.length){
+//     document.write(massH[i]);
+//     document.write(i+'<br>');
+//     document.write('*************')
+//     i++;
+// }
+
+
+// const mass =  [1,2,3,4,5,6,7,8,9,10]
+// for (i=0;i<mass.length;i++){
+//     console.log(mass[i])
+// }
+
+// const mass = ['one', 'two','three','four','five','six','sever','eight','nine','ten'];
+// for (i=0;i<mass.length;i++){
+//     console.log(mass[i]);
+// }
+
+// const mass = ['allex', 'asdsa','fsaf','safasf',123,455,234,123,true,false];
+// for (i=0;i<mass.length;i++){
+//     console.log(mass[i]);
+// }
+
+// const mass = ['allex', 'asdsa','fsaf','safasf',123,455,234,123,true,false];
+// for (i=0;i<mass.length;i++){
+//     if(typeof mass[i] === "boolean"){
+//         console.log(mass[i])
+//     }
+// }
+
+// const mass = ['allex', 'asdsa','fsaf','safasf',123,455,234,123,true,false];
+// for (i=0;i<mass.length;i++){
+//     if(typeof mass[i] === "number"){
+//         console.log(mass[i])
+//     }
+// }
+
+// const mass = ['allex', 'asdsa','fsaf','safasf',123,455,234,123,true,false];
+// for (i=0;i<mass.length;i++){
+//     if(typeof mass[i] === "string"){
+//         console.log(mass[i])
+//     }
+// }
+//
+// const mass = ['','','','','','','','','','',];
+// mass.splice(0, 0, 'mama');
+// mass.splice(1, 1, 'dady');
+// mass.splice(2, 2, 'allex');
+// mass.splice(3, 3, 'amax');
+// mass.splice(4, 4, 123);
+// mass.splice(5, 5, 343);
+// mass.splice(6, 6, 324);
+// mass.splice(7, 7, 555);
+// mass.splice(8, 8, true);
+// mass.splice(9, 9, false);
+//
+// console.log(mass)
+
+// for (i=1;i<=10;i++){
+//     console.log(i)
+//     document.write(i)
+// }
+
+// for (i=1;i<=100;i++){
+//     console.log(i);
+//     document.write(i);
+// }
+
+// for (i=1;i<=100;i+=2){
+//     console.log(i);
+//     document.write(i);
+// }
+
+// for (i=2;i<=101;i+=2){
+//     console.log(i);
+//     document.write(i);
+// }
+
+// for (i=1;i<=100;i+=2){
+//     console.log(i);
+//     document.write(i);
+// }
+
+///////CLOCK//////////ХВИЛНИ??????????????????????
+// for (i=1;i<=2;i++ ){
+//     console.log(i)
+// }
+
+
+///СЕКУНДИ///////?????????????????????
+// for (i=1; i<60; i++){
+//     console.log(i);
+// }
+// for (i=1; i<60; i++){
+//     console.log(i);
+// }
+
+/////CLOCK 2////////???????????????????????????????HOURS
+// for (i=1;i<=2; i++){
+//     console.log(i);
+// }
+
+//////////////////////////////MINUTES////////////////////
+
+// for (j=1;j<=2;j++) {
+//     for (i = 1; i < 60; i++) {
+//         console.log(i);
+//     }
+// }
+// for (i=1;i<=20;i++){
+//     console.log(i)
+// }
+
+/////////////////////////////SECONDS/////////////////////
+// for (a=1;a<=2;a++) {
+//     for (j = 1; j <= 60; j++) {
+//         for (i = 1; i < 60; i++) {
+//             console.log(i);
+//         }
+//     }
+// }
+// for (i=1;i<=20;i++) {
+//     for (i = 1; i <= 60; i++) {
+//         console.log(i)
+//     }
+// }
