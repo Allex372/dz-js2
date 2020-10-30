30e8ecd64f0be178fa8aeb4a92f609a01d95ec5b
Allex372 <62441924+Allex372@users.noreply.github.com>
2020-10-30 11:38:03 +0200

    clock

--- main.js
+++ main.js
@@ -8,66 +8,65 @@
 // const mass = [1,2,3,4,5];
 // console.log(mass[4]);
 
-///////////////
-// НЕЗНАЮ///////////////
-
-// const massDiv = ['<div>text</div>','<div>2</div>','<div>3</div>','<div>4</div>','<div>5</div>','<div>6</div>','<div>7</div>','<div>8</div>','<div>9</div>','<div>10</div>']
+// ///////////////
+// // НЕЗНАЮ///////////////
 //
-// for (let i=0; i<massDiv.length; i++){
-//     document.write(massDiv[i])
-// }
-
-// const massDiv = ['<div>text</div>','<div>2</div>','<div>3</div>','<div>4</div>','<div>5</div>','<div>6</div>','<div>7</div>','<div>8</div>','<div>9</div>','<div>10</div>']
+// // const massDiv = ['<div>text</div>','<div>2</div>','<div>3</div>','<div>4</div>','<div>5</div>','<div>6</div>','<div>7</div>','<div>8</div>','<div>9</div>','<div>10</div>']
+// //
+// // for (let i=0; i<massDiv.length; i++){
+// //     document.write(massDiv[i])
+// // }
 //
-// for (let i=0; i<massDiv.length; i++){
-//     document.write(massDiv[i])
-//     document.write(i+'<br>')
-//     document.write('************')
-// }
-
-// const massH = ['<h1>1</h1>','<h1>2</h1>','<h1>3</h1>','<h1>4</h1>','<h1>5</h1>','<h1>6</h1>','<h1>7</h1>','<h1>8</h1>','<h1>9</h1>','<h1>10</h1>','<h1>11</h1>','<h1>12</h1>','<h1>13</h1>','<h1>14</h1>','<h1>15</h1>','<h1>16</h1>','<h1>17</h1>','<h1>18</h1>','<h1>19</h1>','<h1>20</h1>']
+// // const massDiv = ['<div>text</div>','<div>2</div>','<div>3</div>','<div>4</div>','<div>5</div>','<div>6</div>','<div>7</div>','<div>8</div>','<div>9</div>','<div>10</div>']
+// //
+// // for (let i=0; i<massDiv.length; i++){
+// //     document.write(massDiv[i], i+'<br>','****')
+// //
+// // }
 //
-// let i = 0;
+// // const massH = ['<h1>1</h1>','<h1>2</h1>','<h1>3</h1>','<h1>4</h1>','<h1>5</h1>','<h1>6</h1>','<h1>7</h1>','<h1>8</h1>','<h1>9</h1>','<h1>10</h1>','<h1>11</h1>','<h1>12</h1>','<h1>13</h1>','<h1>14</h1>','<h1>15</h1>','<h1>16</h1>','<h1>17</h1>','<h1>18</h1>','<h1>19</h1>','<h1>20</h1>']
+// //
+// // let i = 0;
+// //
+// // while (i<massH.length){
+// //     document.write(massH[i]);
+// //     document.write(i+'<br>');
+// //     document.write('*************')
+// //     i++;
+// // }
 //
-// while (i<massH.length){
-//     document.write(massH[i]);
-//     document.write(i+'<br>');
-//     document.write('*************')
-//     i++;
-// }
-
-
+//
 // const mass =  [1,2,3,4,5,6,7,8,9,10]
-// for (i=0;i<mass.length;i++){
+// for (let i=0;i<mass.length;i++){
 //     console.log(mass[i])
 // }
-
+//
 // const mass = ['one', 'two','three','four','five','six','sever','eight','nine','ten'];
-// for (i=0;i<mass.length;i++){
+// for (let i=0;i<mass.length;i++){
 //     console.log(mass[i]);
 // }
-
+//
 // const mass = ['allex', 'asdsa','fsaf','safasf',123,455,234,123,true,false];
-// for (i=0;i<mass.length;i++){
+// for (let i=0;i<mass.length;i++){
 //     console.log(mass[i]);
 // }
-
+//
 // const mass = ['allex', 'asdsa','fsaf','safasf',123,455,234,123,true,false];
-// for (i=0;i<mass.length;i++){
+// for (let i=0;i<mass.length;i++){
 //     if(typeof mass[i] === "boolean"){
 //         console.log(mass[i])
 //     }
 // }
-
+//
 // const mass = ['allex', 'asdsa','fsaf','safasf',123,455,234,123,true,false];
-// for (i=0;i<mass.length;i++){
+// for (let i=0;i<mass.length;i++){
 //     if(typeof mass[i] === "number"){
 //         console.log(mass[i])
 //     }
 // }
-
+//
 // const mass = ['allex', 'asdsa','fsaf','safasf',123,455,234,123,true,false];
-// for (i=0;i<mass.length;i++){
+// for (let i=0;i<mass.length;i++){
 //     if(typeof mass[i] === "string"){
 //         console.log(mass[i])
 //     }
@@ -86,80 +85,95 @@
 // mass.splice(9, 9, false);
 //
 // console.log(mass)
-
-
-// for (i=1;i<=10;i++){
+//
+//
+// for (let i=1;i<=10;i++){
 //     console.log(i)
 //     document.write(i)
 // }
-
-// for (i=1;i<=100;i++){
+//
+// for (let i=1;i<=100;i++){
 //     console.log(i);
 //     document.write(i);
 // }
-
-// for (i=1;i<=100;i+=2){
+//
+// for (let i=1;i<=100;i+=2){
 //     console.log(i);
 //     document.write(i);
 // }
-
-// for (i=2;i<=101;i+=2){
+//
+// for (let i=2;i<=101;i+=2){
 //     console.log(i);
 //     document.write(i);
 // }
-
-// for (i=1;i<=100;i+=2){
+//
+// for (let i=1;i<=100;i+=2){
 //     console.log(i);
 //     document.write(i);
 // }
-
-///////CLOCK//////////ХВИЛНИ??????????????????????
-// for (i=1;i<=2;i++ ){
-//     console.log(i)
+//
+// /////CLOCK//////////ХВИЛНИ??????????????????????
+// for (let i=1;i<=2;i++ ){
+//     console.log(i + ' хвилиина')
 // }
 
-
-///СЕКУНДИ///////?????????????????????
-// for (j=1;j<=2;j++) {
-//     for (i = 1; i < 60; i++) {
-//         console.log(i);
+//
+// /СЕКУНДИ///////?????????????????????
+// for (let j=1;j<=2;j++) {
+//     for (let i = 1; i < 60; i++) {
+//         console.log(i + ' second');
+//     }
+//     if (i=59) {
+//         console.log(j + ' minut')
 //     }
 // }
+//
+// ///CLOCK 2////////???????????????????????????????HOURS
+//
+//
+// ////////////////////////////MINUTES////////////////////
 
-/////CLOCK 2////////???????????????????????????????HOURS
-// for (i=1;i<=2; i++){
-//     console.log(i);
-// }
-
-//////////////////////////////MINUTES////////////////////
-
-// for (j=1;j<=2;j++) {
-//     for (i = 1; i < 60; i++) {
-//         console.log(i);
-//     }
-// }
-// for (i=1;i<=20;i++){
-//     console.log(i)
-// }
-
-/////////////////////////////SECONDS/////////////////////
-// for (a=1;a<=2;a++) {
-//     for (j = 1; j <= 60; j++) {
-//         for (i = 1; i < 60; i++) {
-//             console.log(i);
+// for (let h =1; h<=2;h++){
+//     for (let m = 1;m<60;m++) {
+//         for (let s=1;s<60;s++){
+//             console.log(s)
+//         }
+//         if (s = 59){
+//             console.log(m)
 //         }
 //     }
+//     if (m=59){
+//         console.log(h)
+//     }
+//
+//
 // }
-// for (i=1;i<=20;i++) {
-//     for (i = 1; i <= 60; i++) {
-//         console.log(i)
+// for (let h=1; h<=2;h++){
+//     for (let m=1;m<60;m++){
+//         for (let s = 1; s<60; s++){
+//             console.log(s)
+//         }
+//         if (s === 59){
+//             console.log(m+ ' min')
+//         }
+//     }
+//     if (m === 59){
+//         console.log(h + ' hour')
+//     }
+// }
+// for (let m=1;m<21;m++) {
+//     for (let s = 1; s < 60; s++) {
+//         console.log(s)
+//     }
+//     if (s === 59) {
+//         console.log(m + ' min')
 //     }
 // }
 
-
-/////ДОДАТКОВО/////
+//
+// // /////ДОДАТКОВО/////
 // const mass = ['a','b','c'];
-// for (i=0;i<mass.length;i++){
+// for (let i=0; i<mass.length; i++){
 //     console.log(mass[i])
 //     document.write(mass[i])
 // }
@@ -170,9 +184,116 @@
 //     document.write(mass[i])
 //     i++;
 // }
-//
+// //
 // const mass = ['a', 'b', 'c'];
 // for (let item of mass){
 //     document.write(item);
-//     console.log(item)
+//
 // }
+
+
+
+///CLAASS/////
+//- Дан масив ['a', 'b', 'c']. Додайте йому в кінець елементи 1, 2, 3 за допомогою циклу.
+
+// const mass = ['a', 'b', 'c']
+// for (let i = 1;i<=3;i++){
+//     mass.push(i)
+// }
+// console.log(mass)
+
+
+//- Дан масив [1, 2, 3]. Зробіть з нього новий масив [3, 2, 1].
+// let mass = [1,2,3]
+// mass.splice(0,1,3)
+// mass.splice(2,3,1)
+// console.log(mass)
+
+
+//- Дан масив [1, 2, 3]. Додайте йому в кінець елементи 4, 5, 6.
+// const mass = [1,2,3];
+// mass.push(4,5,6)
+// console.log(mass)
+
+
+//- Дан масив [1, 2, 3]. Додайте йому в початок елементи 4, 5, 6.
+// const mass = [1,2,3];
+// mass.unshift(4,5,6)
+// console.log(mass)
+
+
+//- Дан масив ['js', 'css', 'jq']. Виведіть на екран перший елемент за допомогою shift()
+// const mass = ['js','css','jq'];
+// let x= mass.shift('js')
+// console.log(x)
+
+
+//- Дан масив ['js', 'css', 'jq']. Виведіть на екран останній елемент за допомогою pop()
+// const mass = ['js','css','jq'];
+// let x= mass.pop('jq')
+// console.log(x)
+
+
+//- Дан масив [1, 2, 3, 4, 5]. За допомогою методу/функції slice перетворіть масив в [4, 5].
+// const mass= [1,2,3,4,5]
+// console.log(mass.slice(3))
+
+
+//- Дан масив [1, 2, 3, 4, 5]. За допомогою методу/функції slice перетворіть масив в [1,2].
+// const mass= [1,2,3,4,5]
+// console.log(mass.slice(0,2))
+
+
+//- Дан масив [1, 2, 3, 4, 5]. За допомогою методу/функції splice перетворіть масив в [1, 4, 5].
+
+// const mass= [1,2,3,4,5]
+// mass.splice(1,1)
+// mass.splice(1,1)
+// console.log(mass)
+
+
+//- Дан масив [1, 2, 3, 4, 5]. За допомогою методу/функції splice зробіть з нього масив [1, 2, 3, 'a', 'b', 'c', 4, 5].
+// const mass = [1,2,3,4,5]
+// mass.splice(3,0,'a','b')
+// mass.splice(5,0,'c')
+// console.log(mass)
+
+
+//- Дан масив [1, 2, 3, 4, 5]. За допомогою методу/функції splice зробіть з нього масив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
+// const mass =  [1,2,3,4,5]
+// mass.splice(1,0,'a','b');
+// mass.splice(6,0,'c')
+// mass.splice(8,0,'e')
+// console.log(mass)
+
+
+//- Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
+
+// const mass = [1,2,3,4,5,6,7,8,9,10]
+//
+// for (let i=2;i<=10;i+=2){
+//     console.log(i)
+// }
+
+
+//- Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу та push () скопіювати значення одного масиву в інший
+
+// const mass1=[1,2,3,4,5,6,7,8,9,10] ------------Не знаю чи такий варіант підходить для умови
+// const mass2 = []
+// mass2.push(mass1)
+// console.log(mass2)
+
+// const mass1 = [1,2,3,4,5,6,7,8,9,10]
+// const mass2 = []
+// for (let i = 1; i<=mass1.length;i++){
+//     mass2.push(i)
+// }
+// console.log(mass2)
+
+//- Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
+
+
+
+
+
+
